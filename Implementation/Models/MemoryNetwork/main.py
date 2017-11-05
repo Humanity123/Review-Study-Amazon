7c2c6ddcdbe09baeb14dafeb41490db69d77e178
Humanity123 <yashagrwl.iitkgp@gmail.com>
2017-11-05 11:01:01 +0530

    Adding data parsers and complete model

--- Implementation/Models/MemoryNetwork/main.py
+++ Implementation/Models/MemoryNetwork/main.py
@@ -1,7 +1,11 @@
 import os 
 import tensorflow as tf
+import pprint
 
+from data import read_data
 from model import MemN2N
+
+pp = pprint.PrettyPrinter()
 
 flags = tf.app.flags
 
@@ -17,15 +21,48 @@ flags.DEFINE_boolean("is_test", False, "Boolean: False for training phase; True 
 
 FLAGS = flags.FLAGS
 
-def main():
+def main(obj):
 	if not os.path.exists(FLAGS.checkpoint_dir):
       os.makedirs(FLAGS.checkpoint_dir)
 	
-	train_data = read('%s/%s.train.txt'%(FLAGS.data_dir, FLAGS.data_name))
-	validation_data = read('%s/%s.validation.txt'%(FLAGS.data_dir, FLAGS.data_name))
-	test_data = read('%s/%s.test.txt'%(FLAGS.data_dir, FLAGS.data_name))
+	count_c = []
+    word2idx_c = {}
+	
+	count_t = []
+	word2idx_t = {}
 
+	train_data_t = read_data('%s/%s.train_t.txt'%(FLAGS.data_dir, FLAGS.data_name), count_t, word2idx_t)
+	validation_data_t = read_data('%s/%s.validation_t.txt'%(FLAGS.data_dir, FLAGS.data_name), count_t, word2idx_t)
+	test_data_t = read_data('%s/%s.test_t.txt'%(FLAGS.data_dir, FLAGS.data_name), count_t, word2idx_t)
 
+	train_data_c = read_data('%s/%s.train_c.txt'%(FLAGS.data_dir, FLAGS.data_name), count_c, word2idx_c)
+	validation_data_c = read_data('%s/%s.validation_c.txt'%(FLAGS.data_dir, FLAGS.data_name), count_c, word2idx_c)
+	test_data_c = read_data('%s/%s.test_c.txt'%(FLAGS.data_dir, FLAGS.data_name), count_c, word2idx_c)
+
+	train_data_y_t = read('%s/%s.train_y_t.txt'%(FLAGS.data_dir, FLAGS.data_name))
+	validation_data_y_t = read('%s/%s.validation_y_t.txt'%(FLAGS.data_dir, FLAGS.data_name))
+	test_data_y_t = read('%s/%s.test_y_t.txt'%(FLAGS.data_dir, FLAGS.data_name))
+
+	train_data_y_p = read('%s/%s.train_y_p.txt'%(FLAGS.data_dir, FLAGS.data_name))
+	validation_data_y_p = read('%s/%s.validation_y_p.txt'%(FLAGS.data_dir, FLAGS.data_name))
+	test_data_y_p = read('%s/%s.test_y_p.txt'%(FLAGS.data_dir, FLAGS.data_name))
+
+	idx2word_c = dict(zip(word2idx_c.values(), word2idx_c.keys()))
+	idx2word_t = dict(zip(word2idx_t.values(), word2idx_t.keys()))
+
+	FLAGS.N_voc = len(word2idx_c)
+	FLAGS.N_target = len(word2idx_t)
+ 	
+ 	pp.pprint(flags.FLAGS.__flags)
+
+    with tf.Session() as sess:
+        model = MemN2N(FLAGS, sess)
+        model.build_model()
+
+        if FLAGS.is_test:
+            model.run((validation_data_t, validation_data_c, validation_y_t, validation_y_p), (test_data_t, test_data_c, test_y_t, test_y_p))
+        else:
+            model.run((train_data_t, train_data_c, train_y_t, train_data_y_p), validation_data_t, validation_data_c, validation_y_t, validation_y_p))
 
 if __name__ == "__main__":
 	tf.app.run()
--- Implementation/Models/MemoryNetwork/model.py
+++ Implementation/Models/MemoryNetwork/model.py
@@ -1,20 +1,29 @@
 import tensorflow as tf
+import numpy as np
 import random
 
 
 class model(object):
-	def __init__(self, config):
+	def __init__(self, config, sess):
 		''' constructor for Memory Neotwork Model '''
 		self.batch_size = config.batch_size
+		self.N_target   = config.N_target
+		self.N_voc      = config.N_voc
 		self.lmda       = config.lmda
 		self.init_std   = config.int_dev
 		self.hops       = config.hops 	
 		self.dim        = config.dim
+		self.sess       = sess
+		self.step       = 0
 	
 	def build_model(self):
 		''' builds the memory network by initialising all the weights '''
+		self.global_step = tf.Variable(0, name="global_step")
+
 		self.q = tf.placeholder(tf.float32, [None, 1], name="q")
 		self.x = tf.placeholder(tf.float32, [None, None, 1], name="x")
+		self.label_t = tf.placeholder(tf.placeholder, [None, 1], name="label_t")
+		self.label_p = tf.placeholder(tf.placeholder, [None, 1], name="label_p")
 
 		self.B = tf.Variable(tf.random_normal([self.dim, self.N_target], stddev=self.init_std), name = 'B')
 		self.A_t = tf.Variable(tf.random_normal([self.dim, self.N_voc], stddev=self.init_std), name = 'A_t')
@@ -57,7 +66,7 @@ class model(object):
 			z = tf.add(o_t, tf.transpose(tf.matmul(self.H_t, tf.transpose(u))))
 			u = tf.nn.sigmoid(z)
 
-		y_t = tf.nn.softmax(tf.transpose(tf.matmul(self.W_t, tf.transpose(u))))
+		self.y_t = tf.nn.softmax(tf.transpose(tf.matmul(self.W_t, tf.transpose(u))))
 
 		x_embed_A_p = tf.nn.embedding_lookup(tf.transpose(self.A_p), self.x)
 		x_embed_reshaped_A_p = tf.reshape(x_embed_A_p, [-1, self.dim])
@@ -68,7 +77,6 @@ class model(object):
 		c_p = tf.reshape(tf.transpose(tf.matmul(self.Vq_t, tf.transpose(x_embed_reshaped_C))), [-1, tf.shape(self.x)[1], self.dim])
 		
 		a_t_avg = tf.scalar_mul(1.0/self.hops, a_t_avg)
-
 		a_t_avg = tf.divide(tf.cumsum(a_t_avg, axis = 1), tf.cast(tf.range(1, tf.shape(a_t_avg)[1]+1), tf.float32))
 
 		for hop in range(self.hops):
@@ -83,6 +91,88 @@ class model(object):
 			z = tf.add(o_p, tf.transpose(tf.matmul(self.H_p, tf.transpose(u))))
 			u = tf.nn.sigmoid(z)
 
-		y_p = tf.nn.softmax(tf.transpose(tf.matmul(self.W_p, tf.transpose(u))))
+		self.y_p = tf.nn.softmax(tf.transpose(tf.matmul(self.W_p, tf.transpose(u))))
 
-		return (y_t, y_p)
+		cross_entropy_loss_t = tf.nn.softmax_cross_entropy_with_logits(labels = self.label_t, logits = self.y_t)
+		cross_entropy_loss_p = tf.nn.softmax_cross_entropy_with_logits(labels = self.label_p, logits = self.y_p)
+		cross_entropy_conditional_loss_p = tf.multiply(tf.transpose(tf.gather(tf.transpose(self.label_t), [1])), cross_entropy_loss_p)
+		
+		self.loss = -1 * tf.reduce_sum(tf.add(cross_entropy_loss_t, cross_entropy_conditional_loss_p))
+
+		self.optim = tf.train.AdamOptimizer(learning_rate=0.001, beta1=0.9, beta2=0.999, epsilon=1e-08).minimize(loss = loss, global_step = self.global_step)
+
+		tf.global_variables_initializer().run()
+		self.saver = tf.train.Saver()
+	
+
+	def train(self, data, val_data):
+		''' trains the model on the given data '''
+		num_batches = int(math.ceil(len(data) / self.batch_size))
+
+		for epoch in range(self.epochs):
+			loss_list = []
+			for batch in data[::self.batch_size]:
+				train_q, train_c, train_y_t, train_y_p = data
+				 _, loss, self.step = self.sess.run([self.optim, self.loss, self.global_step], feed_dict={
+                                                    self.q: train_q,
+                                                    self.c: train_c,
+                                                    self.y_t: train_y_t,
+                                                    self.y_p: train_y_p})
+				loss_list.append(loss)
+			
+			if epoch % 1 == 0:
+				val_loss = self.test(val_data)
+
+				state = {
+					'Loss Avg': np.mean(loss_list)
+					'Loss std_dev': np.std(loss_list)
+					'Val loss': val_loss
+				}
+				print state
+
+			if epoch % 5 == 0:
+                self.saver.save(self.sess,
+                                os.path.join(self.checkpoint_dir, "MemN2N.model"),
+                                global_step = self.step.astype(int))
+
+    def test(self, data):
+    	''' tests the model on the given data'''
+    	num_batches = int(math.ceil(len(data) / self.batch_size))
+
+		for batch in data[::self.batch_size]:
+			test_q, test_c, test_y_t, test_y_p = data
+			loss = self.sess.run([self.loss], feed_dict={
+                                                self.q: test_q,
+                                                self.c: test_c,
+                                                self.y_t: test_y_t,
+                                                self.y_p: test_y_p})
+
+		return loss
+
+	def run(self, train_data, test_data):
+		''' a function to train or test the memory network depending on the is_test flag'''
+        if not self.is_test:
+        	self.train(train_data, test_data)
+            train_loss = np.sum(self.train(train_data))
+            test_loss = np.sum(self.test(test_data, label='Validation'))
+      
+        else:
+            self.load()
+
+            valid_loss = self.test(train_data)
+            test_loss  = self.test(test_data)
+
+            state = {
+                'valid_perplexity': math.exp(valid_loss),
+                'test_perplexity': math.exp(test_loss)
+            }
+            print state
+	
+	def load(self):
+		''' loads the latest saved checkpoint '''
+        print(" [*] Reading checkpoints...")
+        ckpt = tf.train.get_checkpoint_state(self.checkpoint_dir)
+        if ckpt and ckpt.model_checkpoint_path:
+            self.saver.restore(self.sess, ckpt.model_checkpoint_path)
+        else:
+            raise Exception(" [!] Trest mode but no checkpoint found")
