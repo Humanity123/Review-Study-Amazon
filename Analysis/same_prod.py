ae03d5e43be405f3499fddd6f3aa98ac5a47f650
kushagraaa <kushagra.iitkgp@gmail.com>
2017-12-10 02:36:07 +0530

    adding product sim based on id and titles bow

--- Analysis/same_prod.py
+++ Analysis/same_prod.py
@@ -6,27 +6,31 @@ from nltk.tokenize import word_tokenize
 sys.path.append("../Data_Extraction/2.product_metadata/")
 import products_db as my_db
 
-# cmd_arg = sys.argv
-# database_dir = cmd_arg[1]
-database_dir = "/home/kushagra/Documents/BTP/my_work/data"
+cmd_arg = sys.argv
+root_dir = cmd_arg[1]
+database_dir = root_dir + "/data/phone_data"
+# database_dir = "/home/kushagra/Documents/BTP/my_work/data/phone_data"
 us_db = database_dir + "/us/products.db"
 in_db = database_dir + "/in/products.db"
 uk_db = database_dir + "/uk/products.db"
-
-dbs = [in_db, us_db]
-domains = ["IN", "COM"]
+ca_db = database_dir + "/ca/products.db"
+dbs = [in_db, uk_db, us_db, ca_db]
+domains = ["IN", "UK", "COM", "CA"]
 
 prod_table_name = "PROD"
 desc_table_name = "DESC"
 
 def get_features(raw_documents):
+	'''Train model on the set of bow of titles'''
 	gen_docs = [[w.lower() for w in word_tokenize(text)] for text in raw_documents]
 	dictionary = gensim.corpora.Dictionary(gen_docs)
 	corpus = [dictionary.doc2bow(gen_doc) for gen_doc in gen_docs]
 	tf_idf = gensim.models.TfidfModel(corpus)
-	sims = gensim.similarities.Similarity('/home/kushagra/Documents/BTP/my_work/Review-Study-Amazon/Analysis/', tf_idf[corpus], num_features=len(dictionary))
+	sims = gensim.similarities.Similarity('./', tf_idf[corpus], num_features=len(dictionary))
 	return sims
+
 def get_ids_reviews():
+	'''extracts ProductId, Title and Domain from the database'''
 	raw_documents = []
 	ids = []
 	dns = []
@@ -41,20 +45,7 @@ def get_ids_reviews():
 
 		while row:
 			ids.append(row[0])
-			# desc_table = my_db.database_sqlite()
-			# desc_table.create_connection(dbs[i])
-			# desc_table.set_table_name(domains[i])
-			# #initialise a cursor in db
-			# desc_table.initialise_cursor_description(desc_table_name,row[0])
-
-			# descs = ""
-			# row_i = desc_table.get_next_element()
-			# while row_i:
-			# 	descs = descs + " " + row_i[1]
-			# 	row_i = desc_table.get_next_element()
-
 			raw_documents.append(row[1])
-			
 			dns.append(domains[i])
 			row = metadata_table.get_next_element()
 
@@ -62,6 +53,7 @@ def get_ids_reviews():
 	return ids, raw_documents, dns
 
 def get_index_similarity_matrix(sims, dns):
+	'''returns top most similar entries from different domain'''
 	top_sim = []
 	for crt, sim in enumerate(sims):
 		L = [(i[0],i[1]) for i in sorted(enumerate(sim), key=lambda x:x[1], reverse=True)]
@@ -69,21 +61,22 @@ def get_index_similarity_matrix(sims, dns):
 		# top_sim.append(L[:500])
 	return top_sim
 
-
 def main():
 	ids, raw_documents, dns = get_ids_reviews()
 	print("Number of documents:",len(raw_documents))
+
 	print "Getting Bag of Words -> tf-idf"
 	sims = get_features(raw_documents)
 	print "Tf-Idf Retrieved..."
+
 	print "Getting similarities matrix...	"
 	ans = get_index_similarity_matrix(sims, dns)
 	print "Got Similarity Matrix"
+
 	print "Getting top matches..."
-	# Matches = [(ids[crt],[(ids[index_sim[0]], index_sim[1]) for index_sim in sims if dns[crt]!=dns[index_sim[0]]][:1]) for crt ,sims in enumerate(ans[0:])]
-	# print ids[sim[0][0]], sim[0][1],"--", ids[sim[1][0]], sim[1][1], "--", ids[sim[2][0]], sim[2][1],"--", ids[sim[3][0]], sim[3][1]
 	matches_f = [ (ids[crt],ids[elem[0][0]], elem[0][1])  for crt, elem in enumerate(ans)]
 	matches_sorted = sorted(matches_f, key=lambda x:x[2], reverse=True) 
+
 	print matches_sorted[:20]
 	print "======================="
 	print matches_sorted[200:220]
@@ -93,6 +86,7 @@ def main():
 	print matches_sorted[600:620]
 	print "======================="
 	print matches_sorted[800:820]
+	
 	print len(matches_sorted)
 
 if __name__ == '__main__':
--- Analysis/same_prod_id.py
+++ Analysis/same_prod_id.py
@@ -8,20 +8,22 @@ import copy
 
 sys.path.append("../Data_Extraction/1.product_links")
 import links_db as my_db
-
+'''input1 destination dir where joined db is stored'''
+'''input2 source dir containing crawled data'''
 cmd_arg = sys.argv
 destination_database_dir = cmd_arg[1]
-database_path_in_links = destination_database_dir + "/in/in_links.db"
-database_path_ca_links = destination_database_dir + "/ca/ca_links.db"
-database_path_uk_links = destination_database_dir + "/uk/uk_links.db"
-database_path_us_links = destination_database_dir + "/us/us_links.db"
 source_database_dir = cmd_arg[2]
-source_database_dir_in = source_database_dir + "/in/links.db"
-source_database_dir_ca = source_database_dir + "/ca/links.db"
-source_database_dir_us = source_database_dir + "/us/links.db"
-source_database_dir_uk = source_database_dir + "/uk/links.db"
 
-def save_common_products():
+def save_common_products(destination_database_dir, source_database_dir):
+	'''generates dbs with id,link for each domain for products with same id across all three domains'''
+	database_path_ca_links = destination_database_dir + "/ca/ca_links.db"
+	database_path_uk_links = destination_database_dir + "/uk/uk_links.db"
+	database_path_us_links = destination_database_dir + "/us/us_links.db"
+
+	source_database_dir_ca = source_database_dir + "/ca/links.db"
+	source_database_dir_us = source_database_dir + "/us/links.db"
+	source_database_dir_uk = source_database_dir + "/uk/links.db"
+
 	conn = my_db.create_connection(source_database_dir_uk)
 	conn.execute("attach '%s' as db1" % (source_database_dir_us) )
 	conn.execute("attach '%s' as db2" % (source_database_dir_ca) )
@@ -31,14 +33,15 @@ def save_common_products():
 	my_db.create_connection(database_path_uk_links)
 	my_db.set_table_name("UK_LINKS_com")
 	my_db.create_table()
-	my_db.get_count()
+	# my_db.get_count()
 
 	for row in cursor:
-		print "-------------------------------"
+		# print "-------------------------------"
 		# print row[0]
 		my_db.insert_link(row[0],row[1])
 
 	my_db.save_changes()
+	my_db.get_count()
 
 	conn = my_db.create_connection(source_database_dir_uk)
 	conn.execute("attach '%s' as db1" % (source_database_dir_us) )
@@ -49,10 +52,10 @@ def save_common_products():
 	my_db.create_connection(database_path_ca_links)
 	my_db.set_table_name("CA_LINKS_com")
 	my_db.create_table()
-	my_db.get_count()
+	# my_db.get_count()
 
 	for row in cursor:
-		print "-------------------------------"
+		# print "-------------------------------"
 		# print row[0]
 		my_db.insert_link(row[2],row[3])
 
@@ -68,15 +71,16 @@ def save_common_products():
 	my_db.create_connection(database_path_us_links)
 	my_db.set_table_name("US_LINKS_com")
 	my_db.create_table()
-	my_db.get_count()
+	# my_db.get_count()
 
 	for row in cursor:
-		print "-------------------------------"
+		# print "-------------------------------"
 		# print row[0]
 		my_db.insert_link(row[4],row[5])
 
 	my_db.save_changes()
 	my_db.get_count()
-save_common_products()
+
+save_common_products(destination_database_dir, source_database_dir)
 
 
